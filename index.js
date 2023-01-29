1ba2656c38d15e87bb4ac68d3a4cdf399e218285
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-01-29 17:00:10 -0500

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  
+  let greeting = " "
+  if (ride <= 400) {
+    greeting = "This one is on me!"
+  } else if (ride <= 2000) {
+    greeting = "That will be twenty bucks."
+  } else if (ride > 2000 && ride <= 2500){
+    greeting = "I will gladly take your thirty bucks."
+  } else {
+    greeting = "No can do."
+  } return greeting;
+
+     }
+
+
+function ternaryCheckCity(city){
+   return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
